028c163dfe6dcaf9d084d5a13c595ef7a96f3cad
theogirl <theogirl@outlook.com>
2015-05-31 20:06:39 -0400

    separate functions

--- js/hotcold-test.js
+++ js/hotcold-test.js
@@ -15,44 +15,45 @@ function newGame() {
 	var userScore = 0;
 	var winner = false;
 	var x = getRandomInt(1, 100); // generate a random integer btwn 1 and 100
+	getGuess();
+	validateNumber();
+	resetInput();
+	compareNumber();
+	updateScore();
+	recordGuesses();
 	
 } // end newGame function
 
-function validateNumber() {
+function getGuess() {
 	$('.userinput').keydown(function(event) { 
-			if (event.which == 13) { // if user presses Enter key
-				var y = $(this).val();
+		if (event.which == 13) { 
+		var y = $(this).val();
+}
+
+function resetInput() {
+	$('.userinput').val(''); // return input field to blank
+	$(this).focus();
+}
+
+function validateNumber() {
 				if (y == '') { // make sure input is not empty
-								alert('Please enter something!');
-								$(this).val(''); // return input field to blank
-								$(this).focus();
+						alert('Please enter something!');
 								}
 				else if (y % 1 !== 0) { // make sure input is not a decimal value
-								alert('Please enter an integer!');
-								$(this).val('');
-								$(this).focus();
+						alert('Please enter an integer!');
 								}
 				else if (isNaN(y)) { // makek sure input is not a string
-								alert('Please enter an actual number!');
-								$(this).val(''); 
-							    $(this).focus();
+						alert('Please enter an actual number!');
 								}
 				else if ((y <= 0) || (y > 100)) { // make sure input is not outside of specified range
-								alert('Please enter a number between 1 and 100 only!');
-								$(this).val('');
-								$(this).focus();
+						alert('Please enter a number between 1 and 100 only!');
 								}
 				else {
-								return(y);
-								$(this).val('');
-								$(this).focus();
-								} // end else
-
-					} // end keydown conditions
-				}) // end keydown event
+						return(y);
+					} 
 } // end validateNumber function
 
-function compareNumber(computer, user) {
+function compareNumber() {
 				if (x === y) {
 					alert('You won!');
 					winner = true;	
@@ -79,6 +80,16 @@ function compareNumber(computer, user) {
 						alert('Frozen Arctic');
 					}
 } // end compareNumber function
+
+function updateScore() {
+	userScore++; // increase their attempts count by 1
+	$('.userScore').text(userScore);
+}
+
+function recordGuesses() {
+	userGuesses.push(y); // add user's guess to array
+	$('p.numbers').text(userGuesses);
+}
 
 
 }); // end document ready
